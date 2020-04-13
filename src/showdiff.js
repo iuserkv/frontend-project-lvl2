aed12519f1c7f75bf25480b79f85c852d3768be7
Kirsanov Viacheslav <kirsanovs@yandex.ru>
2020-04-13 16:09:16 +0300

    Шаг 2. Для работы с утлитой gendiff добавлены аргументы и опции.

--- src/showdiff.js
+++ src/showdiff.js
@@ -5,6 +5,8 @@ const showDiff = (params) => {
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
     .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format <type>', 'output format')
+    .arguments('<firstConfig> <secondConfig>')
     .parse(params);
 };
 
